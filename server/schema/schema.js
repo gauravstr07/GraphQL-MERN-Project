13d4ea154ab3434dbda4bdd0ab2e925eda70bcc9
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-12 15:25:32 +0530

    m

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,9 @@
-const { projects, clients } = require("../sampleData");
+
+
+// Mongoose Models
+const Project = require("../models/Project");
+const Client = require("../models/Client");
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -18,7 +23,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return clients.findById(parent.clientId);
       },
     },
   }),
@@ -41,28 +46,28 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
 
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
   },
